ef215eb0a8113a3e8e7943debbae50d4dc8be0b9
david20571015 <david20571015@gmail.com>
2021-10-06 20:12:42 +0800

    Update: use syntax able to vectorize  by compiler

--- hw1/part2/test2.cpp
+++ hw1/part2/test2.cpp
@@ -12,8 +12,10 @@ void test2(float *__restrict a, float *__restrict b, float *__restrict c, int N)
   for (int i = 0; i < I; i++) {
     for (int j = 0; j < N; j++) {
       /* max() */
-      c[j] = a[j];
-      if (b[j] > a[j]) c[j] = b[j];
+      if (b[j] > a[j])
+        c[j] = b[j];
+      else
+        c[j] = a[j];
     }
   }
 
